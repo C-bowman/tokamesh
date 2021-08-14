b3e5c49a6f7b97ba4958c4ebebbad9c6a3a4f9bc
C-bowman <chris.bowman.physics@gmail.com>
2021-08-14 23:05:10 +0100

    Added the tokamaks module, and mast-u boundary functions

---
+++ tokamesh/tokamaks/__init__.py
@@ -0,0 +1,22 @@
+
+from numpy import load
+import os
+tokamaks_dir = os.path.abspath(__file__)[:-11]
+
+
+def mastu_boundary(lower_divertor=False):
+    """
+    Function for accessing boundary data for the MAST-Upgrade tokamak.
+
+    :param bool lower_divertor: \
+        If set to `True`, then only the boundary for the lower super-x divertor is returned.
+
+    :return R_boundary, z_boundary: \
+         The major-radius and z-height values of the boundary are returned as two 1D
+         numpy arrays `R_boundary` and `z_boundary`.
+    """
+    boundary_data = load(tokamaks_dir+'mastu_boundary_data.npz')
+    if lower_divertor:
+        return boundary_data['R_lower_divertor'], boundary_data['z_lower_divertor']
+    else:
+        return boundary_data['R'], boundary_data['z']
