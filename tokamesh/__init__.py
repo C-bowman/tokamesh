909fc4aa5dbad883ac7ba9cfab96ca248f3523fa
C-bowman <chris.bowman.physics@gmail.com>
2021-12-07 00:00:04 +0000

    Automated PyPI upload on GitHub release

--- tokamesh/__init__.py
+++ tokamesh/__init__.py
@@ -1,4 +1,18 @@
 
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except (ModuleNotFoundError, ImportError):
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("tokamesh")
+except PackageNotFoundError:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = ["__version__"]
+
 from numpy import searchsorted, stack, log2, floor, unique, atleast_1d
 from numpy import arange, linspace, int64, full, zeros, meshgrid, ndarray
 from itertools import product
