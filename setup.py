19ad2506cab102ccf398ac6342a1337fa547100e
Peter Hill <peter.hill@york.ac.uk>
2021-12-10 17:49:50 +0000

    Don't link against libm on Windows

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import sys
+
+libraries = []
+if sys.platform == "linux":
+    libraries.append("m")
+
 
 ext_modules = [
     Extension(
         "tokamesh.triangle.triangulate",
         sources=["tokamesh/triangle/triangulate.pyx", "tokamesh/triangle/triangle.c"],
-        libraries=["m"],
+        libraries=libraries,
         define_macros=[("REAL", "double"), ("TRILIBRARY", None), ("NO_TIMER", None)],
     )
 ]
