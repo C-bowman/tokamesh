d94077415197fe6cab3ee94fc02b2822b90922c4
C-bowman <chris.bowman.physics@gmail.com>
2022-09-15 21:40:50 +0100

    added prototype of FieldAlignedMesh class and associated derivative operators

--- tokamesh/operators.py
+++ tokamesh/operators.py
@@ -1,9 +1,18 @@
-from numpy import array, full, ones, arange, tile, diff, sqrt, concatenate
+from numpy import array, full, ones, arange, tile, diff, sqrt, concatenate, ndarray, zeros
 from scipy.sparse import csc_matrix, csr_matrix
+from scipy.special import factorial
+from scipy.linalg import solve
+from itertools import product
 from tokamesh.geometry import build_edge_map
 
 
-def edge_difference_matrix(R, z, triangles, normalised=False, sparse_format="csr"):
+def edge_difference_matrix(
+        R: ndarray,
+        z: ndarray,
+        triangles: ndarray,
+        normalised=False,
+        sparse_format="csr"
+):
     """
     Generates a sparse matrix which, when operating on a vector of field
     values at each vertex, produces a vector of the differences in those
@@ -53,9 +62,9 @@ def edge_difference_matrix(R, z, triangles, normalised=False, sparse_format="csr
 
 
 def umbrella_matrix(
-    R,
-    z,
-    triangles,
+    R: ndarray,
+    z: ndarray,
+    triangles: ndarray,
     ignore_boundary=True,
     inverse_distance_weighting=True,
     normalised=False,
@@ -141,3 +150,124 @@ def umbrella_matrix(
         return csr_matrix((entry_values, (row_indices, col_indices)), shape=shape)
     elif sparse_format == "csc":
         return csc_matrix((entry_values, (row_indices, col_indices)), shape=shape)
+
+
+def parallel_derivative(
+        R: ndarray,
+        z: ndarray,
+        index_grid: ndarray,
+        order=2,
+        sparse_format="csr"
+):
+    """
+    ...
+
+    :param R: \
+        The major radius of each mesh vertex as a 1D numpy array.
+
+    :param z: \
+        The z-height of each mesh vertex as a 1D numpy array.
+
+    :param index_grid: \
+        ...
+
+    :param order: \
+        The order of the derivative to be estimated. Must be either 1 or 2.
+
+    :param str sparse_format: \
+        Specifies the sparse-matrix format to be used for the output. Pass ``'csr'`` for
+        the ``scipy.sparse.csr_matrix`` format, and ``'csc'`` for the ``scipy.sparse.csc_matrix``
+        format.
+    """
+    shape = (3, (index_grid.shape[0] - 2) * index_grid.shape[1])
+    values = zeros(shape)
+    i_indices = zeros(shape)
+    j_indices = zeros(shape)
+
+    prod = product(range(1, index_grid.shape[0] - 1), range(index_grid.shape[1]))
+    for k, (i, j) in enumerate(prod):
+        inds = index_grid[i - 1 : i + 2, j]
+        dl = sqrt(diff(R[inds])**2 + diff(z[inds])**2)
+        pol_dist = array([0.0, dl[0], dl.sum()])
+        coeffs = get_fd_coeffs(pol_dist, order=order)
+
+        values[:, k] = coeffs
+        i_indices[:, k] = inds[1]
+        j_indices[:, k] = inds
+
+    shape = (R.size, R.size)
+    indices = (i_indices.flatten(), j_indices.flatten())
+    if sparse_format == "csr":
+        return csr_matrix((values.flatten(), indices), shape=shape)
+    elif sparse_format == "csc":
+        return csc_matrix((values.flatten(), indices), shape=shape)
+
+
+def perpendicular_derivative(
+        R: ndarray,
+        z: ndarray,
+        index_grid: ndarray,
+        order=2,
+        sparse_format="csr"
+):
+    """
+    ...
+
+    :param R: \
+        The major radius of each mesh vertex as a 1D numpy array.
+
+    :param z: \
+        The z-height of each mesh vertex as a 1D numpy array.
+
+    :param index_grid: \
+        ...
+
+    :param order: \
+        The order of the derivative to be estimated. Must be either 1 or 2.
+
+    :param str sparse_format: \
+        Specifies the sparse-matrix format to be used for the output. Pass ``'csr'`` for
+        the ``scipy.sparse.csr_matrix`` format, and ``'csc'`` for the ``scipy.sparse.csc_matrix``
+        format.
+    """
+    shape = (3, index_grid.shape[0] * (index_grid.shape[1] - 2))
+    values = zeros(shape)
+    i_indices = zeros(shape)
+    j_indices = zeros(shape)
+
+    prod = product(range(index_grid.shape[0]), range(1, index_grid.shape[1] - 1))
+    for k, (i, j) in enumerate(prod):
+        inds = index_grid[i, j - 1 : j + 2]
+        dl = sqrt(diff(R[inds]) ** 2 + diff(z[inds]) ** 2)
+        pol_dist = array([0.0, dl[0], dl.sum()])
+        coeffs = get_fd_coeffs(pol_dist, order=order)
+
+        values[:, k] = coeffs
+        i_indices[:, k] = inds[1]
+        j_indices[:, k] = inds
+
+    shape = (R.size, R.size)
+    indices = (i_indices.flatten(), j_indices.flatten())
+    if sparse_format == "csr":
+        return csr_matrix((values.flatten(), indices), shape=shape)
+    elif sparse_format == "csc":
+        return csc_matrix((values.flatten(), indices), shape=shape)
+
+
+def get_fd_coeffs(points, order=1):
+    # check validity of inputs
+    if type(points) is not ndarray:
+        points = array(points)
+    n = len(points)
+    if n <= order:
+        raise ValueError(
+            "The order of the derivative must be less than the number of points"
+        )
+    # build the linear system
+    b = zeros(n)
+    b[order] = factorial(order)
+    A = ones([n, n])
+    for i in range(1, n):
+        A[i, :] = points**i
+    # return the solution
+    return solve(A, b)
