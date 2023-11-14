a6e9a07b284b253bd349d460156791498b9f18d5
C-bowman <C-bowman@users.noreply.github.com>
2023-11-14 13:21:20 +0000

    [skip ci] Apply black changes

--- tokamesh/__init__.py
+++ tokamesh/__init__.py
@@ -5,6 +5,7 @@ try:
     __version__ = version("tokamesh")
 except PackageNotFoundError:
     from setuptools_scm import get_version
+
     __version__ = get_version(root="..", relative_to=__file__)
 
 __all__ = ["__version__", "TriangularMesh"]
