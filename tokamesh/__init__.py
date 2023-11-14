404b1bbc28394f67aa66bcdf6a53ef63a4ffc585
C-bowman <chris.bowman.physics@gmail.com>
2023-11-14 13:19:45 +0000

    Removed try/except around importlib.metadata import

--- tokamesh/__init__.py
+++ tokamesh/__init__.py
@@ -1,15 +1,10 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except (ModuleNotFoundError, ImportError):
-    from importlib_metadata import version, PackageNotFoundError
+from importlib.metadata import version, PackageNotFoundError
+from tokamesh.mesh import TriangularMesh
 
 try:
     __version__ = version("tokamesh")
 except PackageNotFoundError:
     from setuptools_scm import get_version
-
     __version__ = get_version(root="..", relative_to=__file__)
-
-from tokamesh.mesh import TriangularMesh
 
 __all__ = ["__version__", "TriangularMesh"]
